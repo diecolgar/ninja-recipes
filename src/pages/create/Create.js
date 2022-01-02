c327cc4805c3b135959775db12cf818e2bea1527
diecolgar <diecolgar@gmail.com>
2022-01-03 00:16:30 +0100

    3 January 2022

---
+++ src/pages/create/Create.js
@@ -0,0 +1,64 @@
+import { useState } from 'react'
+
+// styles
+import './Create.css'
+
+export default function Create() {
+  const [title, setTitle] = useState('')
+  const [method, setMethod] = useState('')
+  const [cookingTime, setCookingTime] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(title, method, cookingTime)
+  }
+
+  return (
+    <div className='create'>
+      <h2 className='page-title'>Add a New Recipe</h2>
+
+      <form onSubmit={handleSubmit}>
+
+        <label>
+          <span>Recipe title:</span>
+          <input
+          type="text"
+          onChange={(e) => setTitle(e.target.value)}
+          value={title}
+          required
+          />
+        </label>
+
+        <label>
+          <span>Recite Ingredients</span>
+          <div className='ingredients'>
+            <input type="text" />
+            <button className='btn'>add</button>
+
+          </div>
+        </label>
+
+        <label>
+          <span>Recipe method:</span>
+          <textarea
+            onChange={(e) => setMethod(e.target.value)}
+            value={method}
+            required
+          />
+        </label>
+
+        <label>
+          <span>Cooking time (minutes):</span>
+          <input
+            type="number"
+            onChange={(e) => setCookingTime(e.target.value)}
+            value={cookingTime}
+          />
+        </label>
+
+        <button className='btn'>submit</button>
+
+      </form>
+    </div>
+  )
+}
